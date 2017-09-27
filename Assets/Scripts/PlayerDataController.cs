9aeb3614ebb26384f67d2f1789708e8ce6291da8
Craig Lewin <CSLewin@gmail.com>
2017-09-27 16:15:47 -0700

    Transition buttons, ambient music, fonts, and STAT DISPLAY!

--- Assets/Scripts/PlayerDataController.cs
+++ Assets/Scripts/PlayerDataController.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerDataController : MonoBehaviour {
 
+
+// Some values to mess with
 string PlayerName = "Nameless Vagabond";
 int PlayerCurrentHealth = 15;
 int PlayerMaximumHealth = 15;
 int CombatSkill = 40;
 int DiplomacySkill = 50;
 int MechanicsSkill = 60;
+// end of values to mess with (and visual separator)
 
-int skill;
-int dicecheck;
+string PlayerStats;
 
+public Text PlayerStatsDisplayer; //only use this to display the player's stats on the screen. 
+
+//A simple percentile dicerolling function that retuns "pass" or "fail" and prints some appropriate text to the console.
 public string Roll (int skill) {
 		int dicecheck = Random.Range (1, 101);
 		if (dicecheck <= skill) {
@@ -28,20 +34,22 @@ public string Roll (int skill) {
 
 	// Use this for initialization
 	void Start () {
+		StartGame();
+	}
 
-		//statblock
-		print ("You are known as '" + PlayerName + "'. Your current health is " + PlayerCurrentHealth + "/" + PlayerMaximumHealth + ".\n" +
-			"Your skills are: Combat (" + CombatSkill + "/100), Diplomacy (" + DiplomacySkill + "/100), and Mechanics (" + MechanicsSkill + "/100).");
+	void StartGame () {
+		PrintStats();
+	}
 
-		print ("You test your Combat Skill.");
-		Roll(CombatSkill);
-
-		print ("You test your Diplomacy Skill.");
-		Roll(DiplomacySkill);
-
-		print ("You test your Mechanics Skill.");
-		Roll(MechanicsSkill);
-
+	void PrintStats() {
+		PlayerStats = 
+			"You are known as '" + PlayerName + "'.\n" +
+			"Your current health is " + PlayerCurrentHealth + "/" + PlayerMaximumHealth + ".\n" +
+			"Your skills are: \n" + 
+			"\tCombat (" + CombatSkill + "%)\n" + 
+			"\tDiplomacy (" + DiplomacySkill + "%)\n" + 
+			"\tMechanics (" + MechanicsSkill + "%).\n";
+		PlayerStatsDisplayer.text = PlayerStats;
 	}
 
 }
